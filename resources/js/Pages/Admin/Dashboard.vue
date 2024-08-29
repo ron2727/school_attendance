efea4b336eb25db6acf243854bf169231c11cb71
John Ron <kikobuere27@gmail.com>
2024-08-29 11:14:30 +0800

    Create dropdown component

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -5,18 +5,25 @@
                 <h1 class=" text-center text-white font-bold">School Attendance</h1>
             </div>
             <div class="profile mr-5">
-                <div class="dropdown">
-                    <div id="trigger">
-                        <span>John Doe</span>
-                    </div>
-                </div>
+                <Dropdown>
+                    <template #trigger>
+                        John Doe
+                    </template>
+                    <template #content>
+                       <div class="py-3 flex flex-col space-y-1">
+                           <Link class=" py-2 px-5 hover:bg-indigo-600 hover:text-white">Manage Profile</Link>
+                           <Link class=" py-2 px-5 hover:bg-indigo-600 hover:text-white">Logout</Link>
+                       </div>
+                    </template>
+                </Dropdown>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+import { Link } from '@inertiajs/vue3';
+import Dropdown from '../Shared/Dropdown.vue';
 </script>
 
  
---
+++ resources/js/Pages/Shared/Dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="dropdown relative">
+        <div
+            @click="show = true" 
+            id="trigger"
+            class=" cursor-pointer">
+            <slot name="trigger"/>
+        </div>
+        <div id="content" v-if="show">
+            <div
+                @click="show = false" 
+                id="overlay" 
+                class=" fixed top-0 left-0 w-screen h-screen overflow-hidden bg-black/10"></div>
+            <div class=" absolute right-0 w-max mt-1 border bg-white rounded-lg">
+                <slot name="content"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const show = ref(false)
+
+</script>
+
+ 
