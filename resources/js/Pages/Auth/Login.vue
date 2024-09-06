5c75468b3fa8deda5481c99b89da958322516211
John Ron <kikobuere27@gmail.com>
2024-09-07 06:54:02 +0800

    Added icon on the login form title

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -2,15 +2,17 @@
     <div class=" w-screen h-screen flex justify-center items-center bg-gray-100">
         <div class="wrapper-login max-w-96 w-full border bg-white rounded-lg overflow-x-hidden">
             <div class=" bg-indigo-600">
-               <h1 class=" text-white text-center text-lg font-bold py-5 font-Rubik">School Attendance</h1>
+               <h1 class="flex items-center justify-center text-white text-lg font-bold py-5 font-Rubik">
+                <box-icon name='school' type='solid' color="white" class="mr-2"></box-icon>
+                 School Attendance
+               </h1>
             </div>
             <div class="wrapper-form px-8 py-5">
                 <form @submit.prevent="handleSubmit()" class=" space-y-2">
                    <InputComponent v-model="form.email" input-label="Email" input-name="email" input-place-holder="Enter email..." :input-error="form.errors.email"/>
                    <InputComponent v-model="form.password" input-label="Password" input-name="password" input-type="password" input-place-holder="Enter password..." :input-error="form.errors.password"/>
                    <br>
-                   <!-- <button class=" w-full mt-3 py-2 text-sm rounded bg-indigo-600 text-white text-center" :disabled="form.processing">Login</button> -->
-                   <ButtonSubmit class="w-full" :processing="form.processing" :text="['Login', 'Log-ining']"/>
+                  <ButtonSubmit class="w-full" :processing="form.processing" :text="['Login', 'Log-ining']"/>
                 </form>
             </div>
         </div>
